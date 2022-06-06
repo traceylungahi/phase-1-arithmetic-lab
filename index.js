609683b7ab43c555c9e1a00adfeaf70652e3de88
traceymmbonelungahi <t.lungahi@gmail.com>
2022-06-06 17:23:37 +0300

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+const num1 = 1; 
+const num2 = 62;
+const multiply = num1 * num2; 
+const random = 50;
+Number.isInteger();
+const num3 = 24;
+const num4 = 5;
+const mod = num3 % num4;
+const max = 20;
+Math.max();
 
-// Write your code here
